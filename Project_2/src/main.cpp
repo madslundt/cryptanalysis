c05efffb69fd5207f7d8d134e275503d58968da4
Marc Marer Thomsen <marcmthomsen@gmail.com>
2014-06-18 13:03:38 +0200

    Added UI

--- Project_2/src/main.cpp
+++ Project_2/src/main.cpp
@@ -4,27 +4,57 @@
 #include <math.h>
 #include <map>
 #include <tr1/unordered_map>
-#include "Rainbowtables.cpp"
+//#include "Rainbowtables.cpp"
 
 using namespace std;
 
 int main()
 {
-    unsigned int chains, chain_length, length;
-    std::tr1::unordered_map<string, string> start_points;
-    std::tr1::unordered_map<string, string> end_points;
-    chains = pow(2.0,18.0);
-    chain_length = pow(2.0,10.0);
-    generateRainbowtables(chains, chain_length, start_points, end_points);
-    saveRainbowtable(start_points);
-    length = start_points.size();
-    cout << "Generated " << length << endl;
-    for (auto it : start_points) {
-        cout << it.first << " => " << it.second << endl;
-    }
+    while (true){
+        char c1;
+        cout << "1: Precomputation phase (Generate Rainbow table)" << endl;
+        cout << "2: Online phase" << endl;
+        cout << "!: Exit program" << endl;
+        cout << "Please enter a value: ";
+        cin >> c1;
 
+
+        switch(c1){
+        case '1':
+        char c2;
+        cout << "The precomputation phase will take a while. Confirm (y/n)?" << endl;
+        cin >> c2;
+        if(c2 == 'y'){
+
+            /*unsigned int chains, chain_length, length;
+            std::tr1::unordered_map<string, string> start_points;
+            std::tr1::unordered_map<string, string> end_points;
+            chains = pow(2.0,18.0);
+            chain_length = pow(2.0,10.0);
+            generateRainbowtables(chains, chain_length, start_points, end_points);
+            saveRainbowtable(start_points);
+            length = start_points.size();
+            cout << "Generated " << length << endl;
+            for (auto it : start_points) {
+                cout << it.first << " => " << it.second << endl;
+            }
+    */
+        }
+        else{
+            break;
+        }
+        case '2':
+
+
+        return 0;
+
+        case '!':
+            return 0;
+        default:
+            cout << "The entered value is not a valid input - try again." << endl;
+        }
+    }
     return 0;
 }
-
 
 
