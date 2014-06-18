02faa17ee4d7d3ad4003626e3a3fa8ed7f21397c
madslundt <madssx@hotmail.com>
2014-06-18 19:27:52 +0200

    Added source and header file for containing variables.

---
+++ Project_2/src/variables.h
@@ -0,0 +1,14 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+#include <string>
+#include <stdlib.h>
+#include <math.h>
+
+extern const std::string hex_digits;
+extern const int BIT_SIZE;
+extern const unsigned int MAX;
+extern const char* SAVE_FILE;
+extern const unsigned int MAX_HEX;
+extern const std::string s;
+extern const std::string u;
+#endif // VARIABLES_H
