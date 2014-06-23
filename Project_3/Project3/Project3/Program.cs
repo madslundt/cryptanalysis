69d268435ee1a21ab2e493098c6cbb17138959b9
madslundt <madssx@hotmail.com>
2014-06-23 13:03:39 +0200

    Added functions

--- Project_3/Project3/Project3/Program.cs
+++ Project_3/Project3/Project3/Program.cs
@@ -4,39 +4,66 @@ namespace Project3
 {
 	class MainClass
 	{
-		private static string[] date = 	{"2009-06-22 00:00:00", "2009-06-23 00:00:00", "2009-06-24 00:00:00", 
-										"2009-06-25 00:00:00", "2009-06-26 00:00:00", 
-										"2009-06-27 00:00:00", "2009-06-28 23:59:59"};
-		private static string[] known_words = {"NSA", "National Security Agency", "Obama", "USA", "Snowden"};
+		private static string start_date = "2009-06-22 00:00:00";
+		private static string end_date = "2009-06-22 23:59:59";
+		private static string[] check_words = {"NSA", "National Security Agency", "Obama", "USA", "Snowden"};
 		public static void Main (string[] args)
 		{
-			String[] lines = loadFile(@"ciphertext_sheet3.txt");
-			foreach (string line in lines)
-			{
-				// Use a tab to indent each line of the file.
-				Console.WriteLine("\t" + line);
+			String text = loadFile(@"ciphertext_sheet3.txt");
+			long start = getTimestamp (start_date);
+			long end = getTimestamp (end_date);
+
+			// Running through all the dates
+			for (long i = start; i < end; i++) {
+
+				foreach (char c in text) {
+					// TODO
+				}
 			}
 		}
 
-		public static String updateFunction(String line) 
+		public static bool check_text()
 		{
-			return line;
+			String text = loadFile(@"decrypttext_sheet3.txt");
+			foreach (string w in text.Split(' ')) 
+			{
+				/*if (check_words.Any (w.Contains)) // Needs to check if word (w) is in check_words 
+				{
+					return true;
+				}*/
+			}
+			return false;
 		}
 
-		public static String decryption(String line)
+		public static int[] generateKeys(int i) 
 		{
-			return line;
+			int[] k;
+			for (int j = 0; j < 16; j++) 
+			{
+				// TODO
+			}
+			return k;
 		}
 
-		public static String[] loadFile(String path) 
+		public static double updateFunction(int s) 
 		{
-			return System.IO.File.ReadAllLines(path);
+			return (69.069 * s + 5) % Math.Pow (2.0, 32.0);
+		}
+
+		public static int encryption(char c, int[] k, int i)
+		{
+			return Convert.ToInt32(c) ^ k[i % 16];
+		}
+
+		public static String loadFile(String path) 
+		{
+			return System.IO.File.ReadAllText(path);
 
 		}
 
-		public static String GetTimestamp(DateTime value)
+		public static long getTimestamp(String date)
 		{
-			return value.ToString("yyyy-MM-dd HH:mm:ss");
+			//return Convert.ToDateTime (date).; // Needs to convert string to date to timestamp.
 		}
 	}
 }
